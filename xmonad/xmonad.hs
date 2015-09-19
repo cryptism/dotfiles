069e18720b3c07070776bce5852b41f4ded6356c
Joe Whittles <joewhittles@gmail.com>
2015-09-19 22:25:46 +0000

    Add some good-ass xserver defaults

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,23 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmproc <- spawnPipe "feh --bg-scale ~/.local/share/backgrounds/bust.png"
+    xmproc <- spawnPipe "xcompmgr -c"
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ defaultConfig
+        { modMask = mod4Mask
+        , terminal = "urxvt -mod mod1"
+        , startupHook = spawn "xset r rate 200 20"
+        , manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off") ]
