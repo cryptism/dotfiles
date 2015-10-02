c193fbb37deda38cdcf43f5ec5dd583176c4a590
Joe Whittles <joewhittles@gmail.com>
2015-10-02 17:22:38 +0000

    Move pidgin into its own workspace

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -4,7 +4,11 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.Cursor(setDefaultCursor)
 import XMonad.Util.EZConfig(additionalKeys)
+
 import System.IO
+
+myManageHook = composeAll [
+    className =? "Pidgin" --> doShift "2:im" ]
 
 main = do
     xmproc <- spawnPipe "feh --bg-scale ~/.local/share/backgrounds/bust.png"
@@ -13,11 +17,14 @@ main = do
     xmonad $ defaultConfig
         { modMask = mod4Mask
         , terminal = "urxvt -mod mod1"
+        , workspaces = ["1:main", "2:im", "3:media"] ++ map show([4..9] ++ [0])
         , startupHook = do
+              spawn "pidgin"
               spawn "xset r rate 200 20"
               setDefaultCursor xC_left_ptr
-        , manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+              spawn "xscreensaver -no-splash"
+        , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
         , logHook = dynamicLogWithPP xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
@@ -25,7 +32,8 @@ main = do
         , normalBorderColor = "#9c4c8c"
         , focusedBorderColor = "#ffffff"
         } `additionalKeys`
-        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+        [ ((mod4Mask .|. shiftMask, xK_z)
+               , spawn "xscreensaver-command -lock; xset dpms force off")
         , ((mod4Mask .|. shiftMask, xK_BackSpace), spawn "chromium")
         , ((mod4Mask .|. shiftMask, xK_numbersign), spawn "emacs")
         ]
