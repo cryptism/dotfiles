cdaf5884d17ff38c1862fbec5f1d9c8e978dfefb
Joe Whittles <hello@joewhittl.es>
2016-01-27 04:28:50 +0000

    Make it a bit better for flycheck

--- .emacs.d/site-lisp/misc.el
+++ .emacs.d/site-lisp/misc.el
@@ -1,4 +1,10 @@
-; urxvt-style keyboard yanking
+;;; package --- summary
+;;; Commentary:
+;;; Code:
 (defun paste-primary-selection ()
+  "Urxvt-style keyboard yanking."
   (interactive)
   (insert (x-get-selection 'PRIMARY)))
+
+(provide 'misc)
+;;; misc.el ends here
