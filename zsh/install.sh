ed263d6d8e0601e64e7ccde5e77ce9755486beb5
Joe Whittles <joe.whittles@blocksure.com>
2017-04-20 09:26:08 +0100

    Updates

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+ln -fs `pwd`/zshrc ~/.zshrc
+ln -fs `pwd`/zshenv ~/.zshenv
+ln -fs `pwd`/zsh_alias ~/.zsh_alias
+
 
